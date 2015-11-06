a7973bdcfbdd0d34d8db0eb48e61f4fd430cc97a
Daniel Levitt <daniel.levitt@guardian.co.uk>
2015-11-06 16:48:18 +0000

    Basic index/config file

---
+++ index.js
@@ -0,0 +1,9 @@
+module.exports = {
+  i18n: {
+    en: require("kakapo-assets/i18n/en.json")
+  },
+  theme: require("kakapo-assets/data/theme.json"),
+  settings: require("kakapo-assets/data/theme.json"),
+  sounds: require("kakapo-assets/data/sounds.json"),
+  gradients: require("kakapo-assets/data/gradients.json")
+};
