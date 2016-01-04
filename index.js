f62bab321328b90e3fb49d0c21aa424f54165ad8
Daniel Levitt <daniel.levitt@guardian.co.uk>
2016-01-04 15:45:16 +0000

    Make playlists available

--- index.js
+++ index.js
@@ -5,5 +5,6 @@ module.exports = {
   theme: require("kakapo-assets/data/theme.json"),
   settings: require("kakapo-assets/data/theme.json"),
   sounds: require("kakapo-assets/data/sounds.json"),
-  gradients: require("kakapo-assets/data/gradients.json")
+  gradients: require("kakapo-assets/data/gradients.json"),
+  playlists: require("kakapo-assets/data/playlists.json")
 };
