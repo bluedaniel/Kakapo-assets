5986b7bcb964a50ad2d17236066f09c9bf7b462f
Daniel Levitt <daniel.levitt@guardian.co.uk>
2016-03-21 23:49:39 +0000

    Remove data files from index

--- index.js
+++ index.js
@@ -2,9 +2,7 @@ module.exports = {
   i18n: {
     en: require("kakapo-assets/i18n/en.json")
   },
-  theme: require("kakapo-assets/data/theme.json"),
   settings: require("kakapo-assets/data/theme.json"),
   sounds: require("kakapo-assets/data/sounds.json"),
-  gradients: require("kakapo-assets/data/gradients.json"),
   playlists: require("kakapo-assets/data/playlists.json")
 };
