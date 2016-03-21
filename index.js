42b39710bfa195129f68ffb8d15d7506164726b8
Daniel Levitt <daniel.levitt@guardian.co.uk>
2016-03-21 23:56:31 +0000

    Another typo

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ module.exports = {
   i18n: {
     en: require("kakapo-assets/i18n/en.json")
   },
-  settings: require("kakapo-assets/data/theme.json"),
+  settings: require("kakapo-assets/data/settings.json"),
   sounds: require("kakapo-assets/data/sounds.json"),
   playlists: require("kakapo-assets/data/playlists.json")
 };
